9a04b2ae90834518c1e5a95b435f2424a6e5e572
dmt041104111003 <daomanhtung4102003@gmail.com @example.com>
2025-06-01 23:07:36 +0700

    update2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -130,19 +130,11 @@ export default function Footer() {
         </div>
         
         <div className="pt-8 border-t border-indigo-800 flex flex-col md:flex-row justify-between items-center">
-          <p className="text-gray-400 text-sm mb-4 md:mb-0">
-            &copy; {currentYear} Aptos Demo. Tất cả các quyền được bảo lưu.
+          <div></div>
+          <p className="text-gray-400 text-sm text-center mx-auto">
+            &copy; {currentYear} UTC-APT | FundChain.
           </p>
-          <div className="flex items-center">
-            <p className="text-gray-400 text-sm mr-4">Demo được xây dựng cho mục đích học tập</p>
-            <div className="flex space-x-2">
-              <a href="#" className="text-gray-400 hover:text-white transition-colors">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </a>
-            </div>
-          </div>
+          <div></div>
         </div>
       </div>
     </footer>
